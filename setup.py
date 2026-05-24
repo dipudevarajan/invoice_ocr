0322c8aae97e3005bdaaaae48c45ee01a9b83941
dipudevarajan <dipudevarajan@gmail.com>
2026-05-24 21:44:50 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="invoice_ocr",
+    version="0.0.1",
+    description="AI OCR Invoice Importer for ERPNext",
+    author="Your Name",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[],
+)
